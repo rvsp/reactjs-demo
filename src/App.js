b383548ba068671fcfcd5ebf1f3b9460e36d5358
Venkatasubramanian <pvenkat.rv@gmail.com>
2020-07-10 12:26:53 +0530

    inital-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import { Table } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = "your-api-url";
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({
+          users: result,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    const { error, users } = this.state;
+    if (error) {
+      return <div>Error: {error}</div>;
+    } else {
+      return (
+        <div className="App">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
