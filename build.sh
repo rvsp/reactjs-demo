258a7015d39aa5599aa8761c49602dd04e0b86b1
suganyaanbalagan123 <133192593+suganyaanbalagan123@users.noreply.github.com>
2023-07-16 00:06:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-
-# Set the Docker image name and tag
 IMAGE_NAME="nginx"
 TAG="latest"
-
-# Build the Docker image
 docker build -t "$IMAGE_NAME:$TAG" .
-
-# Check if the image was successfully built
 if [[ "$(docker images -q "$IMAGE_NAME:$TAG" 2>/dev/null)" == "" ]]; then
   echo "Failed to build the Docker image."
   exit 1
 fi
-
 echo "Docker image '$IMAGE_NAME:$TAG' was built successfully."
 
