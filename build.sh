7c20ac614f5edc76c6bfe80ab81e2d643f862a30
Ubuntu <ubuntu@ip-172-31-41-37.ap-south-1.compute.internal>
2023-08-30 14:23:47 +0000

    add

--- build.sh
+++ build.sh
@@ -2,27 +2,6 @@ set -e
 #making dir for jenkins volume
 mkdir -p work
 
-echo "Do you wish to install Docker and Docker compose? y/n"
-read input
-
-if [[ "$input" == "y" ]];
-then 
-	echo "Updating sysytem"
-	sudo apt update
-
-	echo "Installing docker"
-	sudo apt install docker.io
-
-	echo "Installing docker-compose"
-	sudo apt install docker-compose -y
-
-elif [[ "$input" == "n" ]];
-then 	
-	echo "Exiting the docker installing process"
-else
-    echo "Invalid input. Exiting the Docker installation process"
-fi
-
 echo "Building docker image named test"
 docker build -t test .
 echo "Successfully built docker image"
