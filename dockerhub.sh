359f455c315f11fda335be75c30db741bb16c52a
Ubuntu <ubuntu@ip-172-31-41-37.ap-south-1.compute.internal>
2023-09-06 17:47:47 +0000

    added

--- dockerhub.sh
+++ dockerhub.sh
@@ -6,7 +6,7 @@ if [ "$GIT_BRANCH" == "origin/dev" ]; then
     docker push oootuxed0ooo/dev:test
 elif [ "$GIT_BRANCH" == "origin/master" ]; then
     # Build and push to private DockerHub repo for master branch
-    docker build -t docker push oootuxed0ooo/prod:test .
+    docker build -t oootuxed0ooo/prod:test .
     docker login -u docker push oootuxed0oo -p $DOCKERHUB_PASSWORD
     docker push oootuxed0ooo/prod:test
 fi
