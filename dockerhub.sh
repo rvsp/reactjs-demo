bd1f23d48e3170004768c3e3918e0997cb54f969
Ubuntu <ubuntu@ip-172-31-41-37.ap-south-1.compute.internal>
2023-09-09 18:43:48 +0000

    added

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker login -u oootuxed0oo -p $dockerpass
+docker login -u oootuxed0ooo -p $dockerpass
 if [ "$GIT_BRANCH" == "origin/dev" ]; then
     # Build and push to public DockerHub repo for dev branch
     docker build -t oootuxed0ooo/dev:test .
