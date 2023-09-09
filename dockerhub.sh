1a3384c202e27d7f91d964955ccd8a0bba1a7453
Ubuntu <ubuntu@ip-172-31-41-37.ap-south-1.compute.internal>
2023-09-09 19:35:10 +0000

    added

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 docker login -u oootuxed0ooo -p $dockerpass
+#if code pushed into dev branch in github
 if [ "$GIT_BRANCH" == "origin/dev" ]; then
     # Build and push to public DockerHub repo for dev branch
     docker build -t oootuxed0ooo/dev:test .
     docker push oootuxed0ooo/dev:test
+#if dev branch merged into master in github    
 elif [ "$GIT_BRANCH" == "origin/master" ]; then
     # Build and push to private DockerHub repo for master branch
     docker build -t oootuxed0ooo/prod:test .
