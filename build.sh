4bd5c6ac373889c4160ff557d6b888568fd46b15
adnaan-s <143017760+adnaan-s@users.noreply.github.com>
2023-11-04 23:35:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,5 @@ IMAGE_NAME="practice"
 # Build the Docker image
 docker build -t $IMAGE_NAME .
 
-# Check if the build was successful
-if [ $? -eq 0 ]; then
-  echo "Docker image build successful: $IMAGE_NAME"
-else
-  echo "Docker image build failed"
-fi
+
 
